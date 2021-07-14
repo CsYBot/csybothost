043d5cf4aa93b7effe4cb8f4da82fb2aa6a76513
CsYBot <67687653+CsYBot@users.noreply.github.com>
2021-07-14 21:58:50 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,2 +1,3 @@
 commandcreate("hi", "hello");
+commandcreate("bug", "${pass}")
 csyhost.login('o3b3w324s345e3y4m3b4u3x2x47675w2q3a3y3q4i3y4w4t214z3y423f4n4b4z3o2t5o4w5k41526l3h3j4x2y4o3v42484v4r4q2w304a5v533w2e413');
