8c17b20b8418c3e9fdaf3c5ef4796c28c5324d45
CsYBot <67687653+CsYBot@users.noreply.github.com>
2021-07-14 19:31:21 +0300

    Update index.js

--- index.js
+++ index.js
@@ -10,4 +10,4 @@ client.on('ready', () => {
 
 
 csyhost.logchannel("817086184953741323");
-csyhost.login('puff');
+csyhost.login('o3b38336q34584y2m3r463s2y455r413o3c4u3q2k3g3m5o414z3k4o4j4v393z3s21604n4w306z504c3j36444g4c4l4h3a4n393n3m5r5o5u4w2h3u3');
