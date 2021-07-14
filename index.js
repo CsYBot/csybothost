35da197f91a093def117333dc97968e675681922
CsYBot <67687653+CsYBot@users.noreply.github.com>
2021-07-14 20:54:48 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,3 @@
-const Discord = require('discord.js');
-const client = new Discord.Client();
+commandcreate("test", "test bro")
 
-client.on('ready', () => {
-	console.log(`Logged in as ${client.user.tag}!`);
-});
-
-
-
-
-
-csyhost.logchannel("817086184953741323");
 csyhost.login('o3b3w324s345e3y4m3b4u3x2x47675w2q3a3y3q4i3y4w4t214z3y423f4n4b4z3o2t5o4w5k41526l3h3j4x2y4o3v42484v4r4q2w304a5v533w2e413a');
